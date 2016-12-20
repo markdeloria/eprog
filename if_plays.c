4679aa5a9d62e0beb5eae08da80854baf6e627c1
markdeloria <markpambideloria@gmail.com>
2016-12-20 08:10:23 +0800

    Update if_plays.c

--- if_plays.c
+++ if_plays.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main(){
 	int num;
-	printf("Enter Num ");
-	scanf("%d", &num);
-	if (num%3 == 0){
-	printf("Ed ");	
-	}
-	if (num%5 == 0){
-	printf("pogi");
-	}
+		printf("Enter Num ");
+		scanf("%d", &num);
+		if (num%3 == 0){
+			printf("Ed ");	
+		}
+		if (num%5 == 0){
+			printf("pogi");
+		}
 }
