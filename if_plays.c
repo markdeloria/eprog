522bc0418bf48cf7b857b99c7ad29ffb049c1dcd
markdeloria <markpambideloria@gmail.com>
2016-12-20 08:03:14 +0800

    Create if_plays.c

---
+++ if_plays.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int num;
+	
+	printf("Enter Num ");
+	scanf("%d", &num);
+	
+if (num%3 == 0){
+	printf("Ed ");	
+}
+if (num%5 == 0){
+	printf("pogi");
+}
+}
