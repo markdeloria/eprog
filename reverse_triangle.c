252e880eff9c35d214b83a16d020ecd825ad32cc
markdeloria <markpambideloria@gmail.com>
2017-01-24 07:55:09 +0800

    Create reverse_triangle.c

---
+++ reverse_triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i, j, num;
+    printf("Enter number of num: ");
+    scanf("%d",&num);
+	for(i=1; i<=num; i++){
+        for(j=i; j<=num; j++){
+            printf("*");
+        }
+        printf("\n");
+	}
+}
