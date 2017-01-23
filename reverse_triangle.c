6a54ee0f6de132f2a844c1c2c003122818bb8c8b
markdeloria <markpambideloria@gmail.com>
2017-01-24 07:59:51 +0800

    Update reverse_triangle.c

--- reverse_triangle.c
+++ reverse_triangle.c
@@ -3,12 +3,12 @@
 int main(){
 	
 	int i, j, num;
-    printf("Enter number of num: ");
-    scanf("%d",&num);
+	printf("Enter number: ");
+	scanf("%d",&num);
 	for(i=1; i<=num; i++){
-        for(j=i; j<=num; j++){
-            printf("*");
-        }
-        printf("\n");
+		for(j=i; j<=num; j++){
+			printf("*");
+		}
+		printf("\n");
 	}
 }
